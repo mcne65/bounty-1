52209d141a9fca561df3f1fc72715e3ad83a7c8c
Yavor Stoychev <stoychev.yavor@gmail.com>
2018-08-23 21:14:38 +0300

    deployed to rinkeby

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_ENDPOINT),
+      network_id: 4
     }
   }
 };
